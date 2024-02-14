67daadfd9ab4935640931e86d6b954b32863bb91
veikkovahasoyrinki <veikko.vahasoyrinki@protonmail.ch>
2024-02-14 13:03:45 +0200

    hh

---
+++ the room/Assets/RotateTower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateTower : MonoBehaviour
+{
+    public InputActionReference action;
+    public Vector2 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
