94fe3b9f301837482785eceb052745b9fa0a1f1e
veikkovahasoyrinki <veikko.vahasoyrinki@protonmail.ch>
2024-02-23 13:53:03 +0200

    shooting

--- the room/Assets/LaunchProjectile.cs
+++ the room/Assets/LaunchProjectile.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class LaunchProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform launchPoint;
+    public GameObject projectile;
+    public float launchVel = 10f;
+    public void Shoot()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var _projectile = Instantiate(projectile, launchPoint.position, launchPoint.rotation);
+        _projectile.GetComponent<Rigidbody>().velocity = launchPoint.up * launchVel;
     }
 }
